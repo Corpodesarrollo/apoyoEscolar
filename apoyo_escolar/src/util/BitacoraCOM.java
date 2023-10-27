e4dfc667ef3799c2dd9d96f8ae4901fc11149cea
Giroco <giovanny.romeroc@gmail.com>
2023-10-27 16:57:01 -0500

    cliente para insercion de bitacora

---
+++ apoyo_escolar/src/util/BitacoraCOM.java
@@ -0,0 +1,59 @@
+package util;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+public class BitacoraCOM {
+
+	public static String insertarBitacora(int colegio,int jornada,int modulo,int perfil,int sede,int submodulo,int tipoLog,int usuario, String descripcion){
+		String responseJson = "";
+		String URL = "http://localhost:8080/api/apoyo/consultas/insertarBitacora/";
+		try {
+		//Creamos el cliente de conexión al API Restful
+		      Client client = ClientBuilder.newClient();
+
+		//Creamos el target lo cuál es nuestra URL
+		      WebTarget target = client.target(URL);
+
+		//Creamos nuestra solicitud que realizará el request
+		      Invocation.Builder solicitud = target.request();
+
+		//Creamos y llenamos nuestro objeto BaseReq con los datos que solicita el API
+		      BitacoraDto request = new BitacoraDto();
+		      request.setColegio(colegio);
+		      request.setDescripcion(descripcion);
+		      request.setJornada(jornada);
+		      request.setModulo(modulo);
+		      request.setPerfil(perfil);
+		      request.setSede(sede);
+		      request.setSubmodulo(submodulo);
+		      request.setTipoLog(tipoLog);
+		      request.setUsuario(usuario);
+
+		//Convertimos el objeto req a un json
+		      Gson gson = new Gson();
+		      String jsonString = gson.toJson(request);
+		      System.out.println(jsonString);
+
+		//Enviamos nuestro json vía post al API Restful
+		      Response post = solicitud.post(Entity.json(jsonString));
+
+		//Recibimos la respuesta y la leemos en una clase de tipo String, en caso de que el json sea tipo json y no string, debemos usar la clase de tipo JsonObject.class en lugar de String.class
+		      responseJson = post.readEntity(String.class);
+
+		//Imprimimos el status de la solicitud
+		      System.out.println("Estatus: " + post.getStatus());
+
+		} catch (Exception e) { 
+		//En caso de un error en la solicitud, llenaremos res con la exceptión para verificar que sucedió
+		      System.out.print(e.toString());
+		}
+		return responseJson;
+	}
+}
---
+++ apoyo_escolar/src/util/BitacoraDto.java
@@ -0,0 +1,70 @@
+package util;
+
+public class BitacoraDto {
+	
+	private int usuario;
+	private int modulo;
+	private int submodulo;
+	private int tipoLog;
+	private int colegio;
+	private int jornada;
+	private int sede;
+	private int perfil;
+	private String descripcion;
+	public int getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(int usuario) {
+		this.usuario = usuario;
+	}
+	public int getModulo() {
+		return modulo;
+	}
+	public void setModulo(int modulo) {
+		this.modulo = modulo;
+	}
+	public int getSubmodulo() {
+		return submodulo;
+	}
+	public void setSubmodulo(int submodulo) {
+		this.submodulo = submodulo;
+	}
+	public int getTipoLog() {
+		return tipoLog;
+	}
+	public void setTipoLog(int tipoLog) {
+		this.tipoLog = tipoLog;
+	}
+	public int getColegio() {
+		return colegio;
+	}
+	public void setColegio(int colegio) {
+		this.colegio = colegio;
+	}
+	public int getJornada() {
+		return jornada;
+	}
+	public void setJornada(int jornada) {
+		this.jornada = jornada;
+	}
+	public int getSede() {
+		return sede;
+	}
+	public void setSede(int sede) {
+		this.sede = sede;
+	}
+	public int getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(int perfil) {
+		this.perfil = perfil;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	
+}
