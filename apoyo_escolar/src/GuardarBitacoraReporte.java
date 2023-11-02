b72f56ab62698f009ed2b5c63448e79bfd9180cd
FerManjarres <fmanjarres@mincit.gov.co>
2023-11-01 21:16:07 -0500

    Ajuste para agregar servlet nuevo

--- apoyo_escolar/src/GuardarBitacoraReporte.java
+++ apoyo_escolar/src/GuardarBitacoraReporte.java
@@ -1,18 +1,10 @@
 
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
-
-import siges.login.beans.Login;
-import util.BitacoraCOM;
 
 /**
  * Servlet implementation class GuardarBitacoraReporte
@@ -32,29 +24,7 @@ public class GuardarBitacoraReporte extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String jsonString="";
-		BitacoraCOM com = new BitacoraCOM();
-		try{
-			Login login = (Login)request.getSession().getAttribute("login");
-//			login.setInstId("1");
-//			login.setJornadaId("1");
-//			login.setPerfil("180");
-//			login.setSedeId("1");
-//			login.setUsuarioId("21233646");
-			String nombreArchivo = request.getParameter("archivo");
-			Map<String, String> desc = new HashMap<String, String>();
-			desc.put("Nombre del archivo", nombreArchivo);
-			Gson gson = new Gson();
-			jsonString = gson.toJson(desc);
-			com.insertarBitacora(Long.valueOf(login.getInstId()), Integer.parseInt(login.getJornadaId()), 4, login.getPerfil(), Integer.parseInt(login.getSedeId()), 
-					1002, 5/*eliminacion*/, login.getUsuarioId(), jsonString);	
-		}catch(Exception e){
-			
-		}
-		String newURL = response.encodeRedirectURL(request.getParameter("action"));
-		response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-		response.setHeader("Location", newURL);
+		response.getWriter().write("HOLA MUNDO");
 	}
 
 	/**
